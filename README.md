61894a50907e16deac599e0f704ae9b674eaf1c2
dickeyxxx <jeff@dickeyxxx.com>
2014-10-19 22:59:51 -0700

    removed cruft

---
+++ README.md
@@ -0,0 +1,1 @@
+Clones and gets the latest version of a GitHub repo from the command line.
--- main.go
+++ main.go
@@ -6,34 +6,12 @@ import (
 	"os"
 	"os/exec"
 	"path"
-
-	"code.google.com/p/goauth2/oauth"
-	"github.com/google/go-github/github"
 )
 
-var GithubToken = os.Getenv("GITHUB_TOKEN")
 var GithubRepoOwner = os.Getenv("GITHUB_REPO_OWNER")
 var GithubRepoName = os.Getenv("GITHUB_REPO_NAME")
 var RepoPath = path.Join("tmp", GithubRepoOwner, GithubRepoName)
 var GithubUrl = "git://github.com/" + GithubRepoOwner + "/" + GithubRepoName
-
-func GithubClient(token string) *github.Client {
-	t := &oauth.Transport{
-		Token: &oauth.Token{AccessToken: token},
-	}
-	return github.NewClient(t.Client())
-}
-
-func listCommits() {
-	client := GithubClient(GithubToken)
-	commits, _, err := client.Repositories.ListCommits(GithubRepoOwner, GithubRepoName, nil)
-	if err != nil {
-		panic(err)
-	}
-	for _, commit := range commits {
-		fmt.Println(*commit.Commit.Committer.Date)
-	}
-}
 
 func main() {
 	_, err := os.Stat(RepoPath)
